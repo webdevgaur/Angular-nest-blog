a342c66b0d5e80be47e97918fa811f29fa4799d3
Anurag Gaur <77577314+webdevgaur@users.noreply.github.com>
2023-04-20 17:10:14 +0530

    wip: jwt added

--- api/src/user/user.module.ts
+++ api/src/user/user.module.ts
@@ -3,10 +3,12 @@ import { Module } from '@nestjs/common';
 import { UserService } from './service/user.service';
 import { UserController } from './controller/user.controller';
 import { UserEntity } from './models/user.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([UserEntity])
+    TypeOrmModule.forFeature([UserEntity]),
+    AuthModule
   ],
   providers: [UserService],
   controllers: [UserController]
